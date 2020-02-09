dd5887a16b329e2a36660a232aeb890c42638e49
Animesh Singh <animesh.singh@accemy.com>
2020-02-09 22:28:10 +0530

    Initial Commit

---
+++ repository-manager/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  repository-manager
+//
+//  Created by Animesh Singh on 09/02/20.
+//  Copyright © 2020 Animesh Singh. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
